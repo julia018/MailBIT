c2b84bff7d993a8a9bdc2b8d8eaec1f46c711be6
Yulia <kisliuk-y@mail.ru>
2020-07-13 08:29:32 +0300

    form toogling

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ Inputmask({
   "mask": "+1(999)999-99-99",
   showMaskOnHover: false
 }).mask(inputsTel);
+
+let formContainer = document.getElementById('form-container');
+let cross = document.getElementById('cross');
+
+let showForm = function() {
+    formContainer.style.display = 'flex';
+}
+
+let closeForm = function() {
+    formContainer.style.display = 'none';
+}
+
+cross.onclick = closeForm;
